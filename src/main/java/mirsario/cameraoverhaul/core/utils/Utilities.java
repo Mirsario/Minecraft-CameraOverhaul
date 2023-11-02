d58738cbd9680e229eb24f1613d0df502e1ef0b2
Mirsario <me@mirsar.io>
2023-11-02 18:56:44 +0300

    Improved version-abstractions' logging.

--- src/main/java/mirsario/cameraoverhaul/core/utils/Utilities.java
+++ src/main/java/mirsario/cameraoverhaul/core/utils/Utilities.java
@@ -15,13 +15,17 @@ public final class Utilities
 				result = suppliers[i].get();
 			}
 			catch (Exception e) {
+				CameraOverhaul.Logger.info("CameraOverhaul: Implementation #" + i + " for abstraction '" + methodName + "' failed with error " + e.getClass().getName() + " - " + e.getMessage());
 				continue;
 			}
 
-			if (result != null) {
-				CameraOverhaul.Logger.info("CameraOverhaul: Using implementation #" + i + " for abstraction '" + methodName + "'.");
-				return result;
+			if (result == null) {
+				CameraOverhaul.Logger.info("CameraOverhaul: Implementation #" + i + " for abstraction '" + methodName + "' returned nothing.");
+				continue;
 			}
+
+			CameraOverhaul.Logger.info("CameraOverhaul: Using implementation #" + i + " for abstraction '" + methodName + "'.");
+			return result;
 		}
 		
 		return null;
