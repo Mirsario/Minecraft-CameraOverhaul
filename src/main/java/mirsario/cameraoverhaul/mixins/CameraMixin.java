8c70970676221cfa3cdc172d9ee9b7c6b5dc2398
Mirsario <me@mirsar.io>
2026-05-25 11:36:23 +0300

    fix: Extend #117 to [1.21-1.21.10] range.

--- src/main/java/mirsario/cameraoverhaul/mixins/CameraMixin.java
+++ src/main/java/mirsario/cameraoverhaul/mixins/CameraMixin.java
@@ -29,7 +29,7 @@ import org.joml.*;
 @Mixin(Camera.class)
 @SuppressWarnings("UnusedMixin")
 public abstract class CameraMixin {
-	//? if >=1.21.11 {
+	//? if >=1.21 {
 	private static final Vector3f FORWARDS = new Vector3f(0, 0, -1);
 	private static final Vector3f UP = new Vector3f(0, 1, 0);
 	private static final Vector3f LEFT = new Vector3f(-1, 0, 0);
@@ -186,12 +186,16 @@ public abstract class CameraMixin {
 		yRot += yaw;
 
 		var quat = rotation();
+
 		//? if >=1.19.3 {
 		quat.premul(new Quaternionf().rotationAxis(yaw * MathUtils.DEG_TO_RAD, 0, -1, 0));
-		//? if >=1.21.11
+
+		//? if >=1.21
 		quat.rotateAxis(pitch * MathUtils.DEG_TO_RAD, -1, 0, 0);
-		//? if <1.21.11
+
+		//? if <1.21
 		/*quat.rotateAxis(pitch * MathUtils.DEG_TO_RAD, 1, 0, 0);*/
+
 		FORWARDS.rotate(quat, forwards);
 		UP.rotate(quat, up);
 		LEFT.rotate(quat, left);
